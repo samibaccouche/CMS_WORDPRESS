dc2a92a0e6ca4d601d1f23a158156b9de2d863ed
samibaccouche <baccouchemedsami@gmail.com>
2026-04-15 23:17:37 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -84,9 +84,6 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
-}
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
