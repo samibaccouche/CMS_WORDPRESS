9887ea187f41e4ec704d2f120b8ce25c95951b6e
samibaccouche <baccouchemedsami@gmail.com>
2026-04-15 23:16:59 +0100

    Remove DB_USER definition from wp-config.php
    Removed database username definition from wp-config.php.

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,6 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'local' );
-
-/** Database username */
-define( 'DB_USER', 'root' );
-
 /** Database password */
 define( 'DB_PASSWORD', 'root' );
 
